91e0457d4781ea08802f9e0d52aa327a4c1ea9b8
Tom Waller <thomas.r.waller@gmail.com>
2021-09-06 16:47:30 +1000

    Create collection work

--- data-warehouse/pokemon_tcg_api_to_mysql.py
+++ data-warehouse/pokemon_tcg_api_to_mysql.py
@@ -4,16 +4,18 @@ from io import BytesIO
 from pathlib import Path
 import urllib.request
 from zipfile import ZipFile
+import zipfile
 import tempfile 
 
-data_source_location = os.path.join(tempfile.gettempdir(), "tcg_api")
+# data_source_location = os.path.join(tempfile.gettempdir(), "tcg_api")
+data_source_location = os.path.join(os.path.dirname(os.path.realpath(__file__)), "tcg_api")
 force_download = False
 
 data_sources = {
     "pokedex": {
         "url": "https://raw.githubusercontent.com/fanzeyi/pokemon.json/master/pokedex.json",
     },
-    "tcg": {
+    "tcg_api": {
         "url": "https://github.com/PokemonTCG/pokemon-tcg-data/archive/refs/heads/master.zip"
     },
 }
@@ -31,7 +33,7 @@ def download_data():
         destination_directory = os.path.join(data_source_location, data_source_name)
         destination_file = os.path.join(destination_directory, f"{data_source_name}{extension}")
 
-        if not Path(destination_file).is_file():
+        if not Path(destination_file).is_file() or (Path(destination_file).is_file() and force_download):
             Path(destination_directory).mkdir(parents=True, exist_ok=True)
             with urllib.request.urlopen(url) as downloaded_data:
                 with open(destination_file,'wb') as output:
@@ -39,20 +41,59 @@ def download_data():
         
         #Load file into memory here
         with open(destination_file) as downloaded_data:
-            if url.endswith("json"):
-                data = json.load(downloaded_data)
+            if extension == ".json":
+                data_dict = json.load(downloaded_data)
                 data_sources[data_source_name]['data'] = data_dict
 
-            elif url.endswith("zip"):
+            elif extension == ".zip":
                 destination_directory_data = os.path.join(destination_directory, 'data')
-                data_sources[data_source_name]['data'] = json.load(f)
 
-                with ZipFile(BytesIO(f.read())) as zfile:
-                    zfile.extractall(destination_directory_data)
+                with zipfile.ZipFile(destination_file, 'r') as zip_ref:
+                    zip_ref.extractall(destination_directory_data)
+
+                if data_source_name == 'tcg_api':
+                    data_sources[data_source_name]['data'] = {}
+
+                    # sets
+                    data_sources[data_source_name]['data']['sets'] = {}
+                    set_path = os.path.join(destination_directory_data, "pokemon-tcg-data-master/sets")
+                    for filename in os.listdir(set_path):
+                        langauge_code = filename.replace(".json", "")
+                        set_language_file = os.path.join(set_path, filename)
+                        data_sources[data_source_name]['data']['sets'][langauge_code] = {}
+
+                        with open(set_language_file) as set_language_downloaded_data:
+                            data_dict = json.load(set_language_downloaded_data)
+                            for set in data_dict:
+                                data_sources[data_source_name]['data']['sets'][langauge_code][set['id']] = set
+
+                    #cards
+                    cards_path = os.path.join(destination_directory_data, "pokemon-tcg-data-master/cards")
+
+                    # For each language
+                    for cards_language in os.listdir(cards_path):
+                        cards_language_dir = os.path.join(cards_path, cards_language)
+
+                        # For each set in language
+                        for set_in_language in os.listdir(cards_language_dir):
+                            set_in_language_dir = os.path.join(cards_language_dir, set_in_language)
+                            set_code = set_in_language.replace(".json", "")
+
+                            with open(set_in_language_dir) as set_data:
+                                set_data = json.load(set_data)
+                                data_sources[data_source_name]['data']['sets'][langauge_code][set['id']]['cards'] = {}
+
+                                for card in set_data:
+                                    data_sources[data_source_name]['data']['sets'][langauge_code][set['id']]['cards'][card['id']] = card
+                                ke = 1
+
+                    #decks
+                    decks_path = os.path.join(destination_directory_data, "pokemon-tcg-data-master/decks")
+
 
                     for filename in os.listdir(destination_directory_data):
                         full_path = os.path.join(destination_directory_data, filename)
-                        with open(full_path):
+                        with open(full_path) as f:
                             data_sources[data_source_name]['data']['filename'] = json.load(f)
 
 
