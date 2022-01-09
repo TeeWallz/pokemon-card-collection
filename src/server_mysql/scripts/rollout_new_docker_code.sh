e432647603daf0406d9fff9d86a2d3d535f8f3c8
Tom Waller <tom.waller@versent.com.au>
2022-01-09 19:04:11 +1100

    Docker

--- src/server_mysql/scripts/rollout_new_docker_code.sh
+++ src/server_mysql/scripts/rollout_new_docker_code.sh
@@ -1,10 +1,5 @@
-https://www.bezkoder.com/react-node-mongodb-auth/
-https://www.bezkoder.com/node-js-mongodb-auth-jwt/
+#!/bin/bash
 
-Same front end?
-
-
-# To push new code
 rm -rf /opt/setup/pokemon-card-collection
 git clone https://github.com/TeeWallz/pokemon-card-collection.git /opt/setup/pokemon-card-collection
 cd /opt/setup/pokemon-card-collection/src/server_mysql
--- src/server_mysql/server.js
+++ src/server_mysql/server.js
@@ -5,7 +5,6 @@ const cors = require("cors");
 const app = express();
 
 var corsOptions = {
-    // origin: "http://localhost:8081"
     origin: "*"
 };
 
